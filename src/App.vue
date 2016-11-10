d0954cd99dd2443afc6d659eead33dd90e11bd81
pranong <nutpat2539@gmail.com>
2016-11-10 21:42:04 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div class="">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  computed: {},
+  mounted () {
+    this.$router.push('/index')
+  },
+  methods: {},
+  components: {}
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/components/imagepage.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+  <div class="">
+    <div class="boxBar">
+      <h1>imagepage</h1>
+      <router-link to="/index">Go to Index</router-link>
+      <router-link to="/albumpage">Go to album</router-link>
+      <router-link to="/imagepage">Go to imagepage</router-link>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  computed: {},
+  mounted () {},
+  methods: {},
+  components: {}
+}
+</script>
+
+<style lang="css">
+.boxBar {
+  border: 2px solid #36d053;
+  width: 50vh;
+}
+</style>
